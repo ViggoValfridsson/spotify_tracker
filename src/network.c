5c2c4f6e83fb8068fb6c0319b18d1ef8885a7757
Viggo Valfridsson <viggovalf@gmail.com>
2025-12-11 20:43:27 +0100

    Fix additional off by one when calculating body_size when URL encoding

--- src/network.c
+++ src/network.c
@@ -11,10 +11,6 @@
 
 #define CREDENTIALS_MAX 1024
 
-typedef struct {
-
-}  ;
-
 int url_encode_kvp(form_key_value_pair *kvp, char **key_out, char **value_out) {
     char *encoded_key;
     char *encoded_value;
@@ -56,7 +52,7 @@ int create_form_url_encoded_body(form_key_value_pair *kvps, int kvp_len, char **
 
         // TODO: Make a cleaner solution for extra chars. This is to account for & and = in result
         int extra_chars = i == 0 ? 1 : 2; // account for = and/or &
-        body_size += encoded_len + extra_chars;
+        body_size += encoded_len + extra_chars + 1; 
         char *tmp = realloc(body, body_size);
 
         if (!tmp) {
